621e76534e60fe8ade8e8806ba21868a1bb7915c
Mon Zacharias <mzacharias@thunderhead.com>
2020-02-20 11:55:56 -0500

    Updated package

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Thunderhead.embeddedframework",
+    name: "Thunderhead",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v8),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Thunderhead.embeddedframework",
-            targets: ["Thunderhead.embeddedframework"])
+            name: "Thunderhead",
+            targets: ["Thunderhead"]),
     ],
     dependencies: [
         // no dependencies
@@ -21,7 +21,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Thunderhead.embeddedframework",
-            dependencies: [])
+            name: "Thunderhead",
+            dependencies: []),
+        .testTarget(
+            name: "ThunderheadTests",
+            dependencies: ["Thunderhead"]),
     ]
 )
